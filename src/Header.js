e5018962b1b83441697ee5838c4e8af0206bd383
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2022-03-27 00:43:39 -0300

    placing main icons

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
- 
-function App() {
+import "./Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+
+function Header() {
   return (
-    <div className="App">
-     <h2> I am a Header</h2>
+    <div className="header">
+        <PersonIcon />
+         <img className='header__logo'
+          src='https://cdn-icons-png.flaticon.com/512/408/408790.png' alt='logo tinder' />
+         <ChatBubbleIcon />
     </div>
   );
 }
 
-export default App;
+export default Header;
